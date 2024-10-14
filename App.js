946b5230df01c22b1a4804309dacc7fca8753d8f
Daniele <rizzodaniele@icloud.com>
2024-10-14 14:47:00 +0100

    hard code login to test, homepage

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './LoginScreen';  // Home screen with login
+import LoginScreen from './LoginScreen';  // Login screen
 import ForgotPasswordScreen from './ForgotPasswordScreen';  // Forgot password screen
-import HomeScreen from './HomeScreen';  // Forgot password screen
-
+import HomeScreen from './HomeScreen';  // Home screen
 
 const Stack = createStackNavigator();
 
@@ -12,14 +11,29 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
+        {/* Login screen (no header, no back arrow) */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}  // Hide header on Login screen
+        />
 
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        {/* Forgot Password screen (with back arrow) */}
+        <Stack.Screen 
+          name="ForgotPassword" 
+          component={ForgotPasswordScreen} 
+          options={{ headerShown: true }}  // Show header with back arrow
+        />
+
+        {/* Home screen (no back arrow) */}
+        <Stack.Screen 
+          name="HomeScreen" 
+          component={HomeScreen} 
+          options={{ 
+            headerLeft: null,  // Hide back arrow on Home screen
+            gestureEnabled: false,  // Disable swipe back gesture (iOS)
+          }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
